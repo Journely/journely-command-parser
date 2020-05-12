7a8795ba682eca0175781bafd591275b70aa8799
Dwamian Mcleish <dmcleish@gmail.com>
2020-05-12 15:34:13 -0500

    - Added more field options to SF

--- src/lib/commands/Commands.test.ts
+++ src/lib/commands/Commands.test.ts
@@ -7,6 +7,7 @@ describe('Commands', function () {
     it('Parse should return an array', function () {
       const Command = new Commands();
       expect(Command.parse('salesforce opportunity:"Something"')).to.be.an('array');
+      expect(Command.parse('1')).to.be.an('array');
     });
   });
 });
