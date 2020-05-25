daae3b4b4078030aeb0e27ece18ba5c64d47c0ba
Dwamian Mcleish <dmcleish@gmail.com>
2020-05-25 15:29:03 -0500

    - Added additional test coverage

--- src/lib/commands/Commands.test.ts
+++ src/lib/commands/Commands.test.ts
@@ -1,13 +1,44 @@
 import Commands from './Commands';
 import 'mocha';
 import { expect } from 'chai';
+import _ from 'lodash';
 
 describe('Commands', function () {
+  const Command = new Commands();
   describe('#parse()', function () {
     it('Parse should return an array', function () {
-      const Command = new Commands();
-      expect(Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value" > gcal')).to.be.an('array');
-      // expect(Command.parse('1')).to.be.an('array');
+      const command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+      expect(command).to.be.an('array');
+    });
+
+    it('Parse data field should be present', function () {
+      const command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+      expect(_.get(command, '[0]data', null)).to.not.be.null;
+    });
+
+    it('Parse config should be present', function () {
+      const command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+      expect(_.get(command, '[0]config', null)).to.not.be.null;
+    });
+
+    it('Parse data.object should be present', function () {
+      const command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+      expect(_.get(command, '[0]data.object', null)).to.not.be.null;
+    });
+
+    it('Parse data.target should be present', function () {
+      const command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+      expect(_.get(command, '[0]data.target', null)).to.not.be.null;
+    });
+
+    it('Parse data.field should be present', function () {
+      const command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+      expect(_.get(command, '[0]data.field', null)).to.not.be.null;
+    });
+
+    it('Parse should return 2 entries', function () {
+      const command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value" > gcal');
+      expect(command.length).to.eq(2);
     });
   });
 });
