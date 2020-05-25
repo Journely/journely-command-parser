c86b6379832b9269ab90531c85198ca6e13f53d2
Dwamian Mcleish <dmcleish@gmail.com>
2020-05-25 15:29:16 -0500

    - build

--- dist/lib/commands/Commands.test.js
+++ dist/lib/commands/Commands.test.js
@@ -6,12 +6,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Commands_1 = __importDefault(require("./Commands"));
 require("mocha");
 var chai_1 = require("chai");
+var lodash_1 = __importDefault(require("lodash"));
 describe('Commands', function () {
+    var Command = new Commands_1.default();
     describe('#parse()', function () {
         it('Parse should return an array', function () {
-            var Command = new Commands_1.default();
-            chai_1.expect(Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value" > gcal')).to.be.an('array');
-            // expect(Command.parse('1')).to.be.an('array');
+            var command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+            chai_1.expect(command).to.be.an('array');
+        });
+        it('Parse data field should be present', function () {
+            var command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+            chai_1.expect(lodash_1.default.get(command, '[0]data', null)).to.not.be.null;
+        });
+        it('Parse config should be present', function () {
+            var command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+            chai_1.expect(lodash_1.default.get(command, '[0]config', null)).to.not.be.null;
+        });
+        it('Parse data.object should be present', function () {
+            var command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+            chai_1.expect(lodash_1.default.get(command, '[0]data.object', null)).to.not.be.null;
+        });
+        it('Parse data.target should be present', function () {
+            var command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+            chai_1.expect(lodash_1.default.get(command, '[0]data.target', null)).to.not.be.null;
+        });
+        it('Parse data.field should be present', function () {
+            var command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value"');
+            chai_1.expect(lodash_1.default.get(command, '[0]data.field', null)).to.not.be.null;
+        });
+        it('Parse should return 2 entries', function () {
+            var command = Command.parse('salesforce object:Opportunity:"Something":"New Something" field:NextStep:"Steps":"Value" > gcal');
+            chai_1.expect(command.length).to.eq(2);
         });
     });
 });
